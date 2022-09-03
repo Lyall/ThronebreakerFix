b2dfef6f3789b53fe88f08258c472ac5f88ee2a3
Lyall <lyall@stamper.net>
2022-09-03 21:06:24 +0100

    Add more fixes.

--- ThronebreakerFix.cs
+++ ThronebreakerFix.cs
@@ -21,6 +21,7 @@ namespace ThronebreakerFix
         public static ConfigEntry<float> fDesiredResolutionY;
         public static ConfigEntry<bool> bFullscreen;
         public static ConfigEntry<bool> bIntroSkip;
+        public static ConfigEntry<string> sAAType;
 
         public override void Load()
         {
@@ -29,15 +30,22 @@ namespace ThronebreakerFix
             Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
 
             // Features
-            bUltrawideFixes = Config.Bind("Ultrawide UI Fixes",
+            bUltrawideFixes = Config.Bind("Features",
                                 "UltrawideFixes",
                                 true,
                                 "Set to true to enable ultrawide UI fixes.");
 
-            bIntroSkip = Config.Bind("Intro Skip",
+            bIntroSkip = Config.Bind("Features",
                                 "IntroSkip",
                                  true,
                                 "Skip intro logos.");
+
+            // Graphics
+            sAAType = Config.Bind("Graphics",
+                                "Anti-Aliasing",
+                                "None",
+                                new ConfigDescription("Set desired anti-aliasing type.",
+                                new AcceptableValueList<string>("None", "FXAA", "SMAA", "TAA"))); // Others are broken/invalid
 
             // Custom Resolution
             bCustomResolution = Config.Bind("Set Custom Resolution",
@@ -64,11 +72,13 @@ namespace ThronebreakerFix
             // Run CustomResolutionPatch
             if (bCustomResolution.Value)
             {
+                Screen.SetResolution((int)fDesiredResolutionX.Value, (int)fDesiredResolutionY.Value, bFullscreen.Value);
+                Log.LogInfo($"Set screen resolution to {fDesiredResolutionX.Value}x{fDesiredResolutionY.Value}, Fullscreen = {bFullscreen.Value}.");
                 Harmony.CreateAndPatchAll(typeof(CustomResolutionPatch));
             }
 
             // Run UltrawidePatch
-            if (bUltrawideFixes.Value)
+            if (bUltrawideFixes.Value && bCustomResolution.Value)
             {
                 Harmony.CreateAndPatchAll(typeof(UltrawidePatch));
             }
@@ -79,20 +89,42 @@ namespace ThronebreakerFix
                 Harmony.CreateAndPatchAll(typeof(IntroSkipPatch));
             }
 
+            Harmony.CreateAndPatchAll(typeof(SettingsPatch));
+
+        }
+
+
+        [HarmonyPatch]
+        public class SettingsPatch
+        {
+            [HarmonyPatch(typeof(GwentUnity.CameraAntiAliasingController), nameof(GwentUnity.CameraAntiAliasingController.Init))]
+            [HarmonyPostfix]
+            public static void ChangeAA(GwentUnity.CameraAntiAliasingController __instance)
+            {
+                var antiAliasing = sAAType.Value switch
+                {
+                    "FXAA" => UnityEngine.Rendering.PostProcessing.PostProcessLayer.Antialiasing.FastApproximateAntialiasing, // FXAA
+                    "SMAA" => UnityEngine.Rendering.PostProcessing.PostProcessLayer.Antialiasing.SubpixelMorphologicalAntialiasing, // SMAA
+                    "TAA" => UnityEngine.Rendering.PostProcessing.PostProcessLayer.Antialiasing.TemporalAntialiasing, // TAA
+                    _ => UnityEngine.Rendering.PostProcessing.PostProcessLayer.Antialiasing.None,
+                };
+
+                if (__instance.m_PostProcessLayer != null)
+                {
+                    __instance.m_PostProcessLayer.antialiasingMode = antiAliasing;
+                }
+
+            }
         }
 
         [HarmonyPatch]
         public class CustomResolutionPatch
         {
-            [HarmonyPatch(typeof(GwentUnity.ResolutionNodeWrapper), nameof(GwentUnity.ResolutionNodeWrapper.ApplyResolution), new Type[] { typeof(int), typeof(int), typeof(bool) })]
+            [HarmonyPatch(typeof(GwentUnity.ResolutionNodeWrapper._HACK_SetResolution_c__Iterator0), nameof(GwentUnity.ResolutionNodeWrapper._HACK_SetResolution_c__Iterator0.MoveNext))]
             [HarmonyPrefix]
-            public static bool ApplyCustomRes(ref int __0, ref int __1, ref bool __2)
+            public static bool ApplyCustomRes()
             {
-                __0 = (int)fDesiredResolutionX.Value;
-                __1 = (int)fDesiredResolutionY.Value;
-                __2 = bFullscreen.Value;
-                Log.LogInfo($"GwentUnity.ResolutionNodeWrapper.ApplyResolution: Resolution set to {__0}x{__1}. Fullscreen = {__2}");
-                return true;
+                return false;
             }
         }
 
@@ -114,13 +146,38 @@ namespace ThronebreakerFix
                 return false;
             }
 
+            // Fix UI Camera aspect ratio
+            [HarmonyPatch(typeof(GwentUnity.AspectRatioManager), nameof(GwentUnity.AspectRatioManager.SetupAspectRatio))]
+            [HarmonyPostfix]
+            public static void AspectRatio(GwentUnity.AspectRatioManager __instance)
+            {
+                __instance.m_RatioToMaintain = NewAspectRatio;
+
+                if (__instance.CurrentViewportData != null)
+                {
+                    var currViewport = __instance.CurrentViewportData;
+                    currViewport.ShouldEnableBlackBars = false;
+                }
+
+                if (CrimsonUI.CrimsonUIManager.Instance != null)
+                {
+                    var UICamera = CrimsonUI.CrimsonUIManager.Instance.UICamera;
+                    UICamera.aspect = NewAspectRatio;
+                    UICamera.pixelRect = new Rect(0f, 0f, 1f, 1f);
+                    UICamera.rect = new Rect(0f, 0f, 1f, 1f);
+                }
+
+                Log.LogInfo($"AspectRatioManager: Adjusted target aspect ratio and UI Camera.");
+            }
+
+
             // Fix resizable UI camera render textures
             [HarmonyPatch(typeof(GwentUnity.RenderTextureManager), nameof(GwentUnity.RenderTextureManager.GetResizableTexture))]
             [HarmonyPrefix]
             public static bool RenTex1(GwentUnity.RenderTextureManager __instance, ref RectTransform __0, ref float __1, ref float __2, ref float __3)
             {
                 __1 = (float)1600 * AspectMultiplier;
-                Log.LogInfo($"ResizableTexture: Resized {__0.name}.");
+                Log.LogInfo($"ResizableTexture: Resized - {__0.name}.");
                 return true;
             }
 
@@ -135,7 +192,7 @@ namespace ThronebreakerFix
                     if (transform != null & transform.sizeDelta == new Vector2(1600f, 900f))
                     {
                         transform.sizeDelta = new Vector2((float)1600 * AspectMultiplier, (float)900);
-                        Log.LogInfo($"ScreenSizeTexture: Resized {transform.name}.");
+                        Log.LogInfo($"ScreenSizeTexture: Resized - {transform.name}.");
                     }
                 }        
             }
@@ -161,26 +218,26 @@ namespace ThronebreakerFix
                     var overlayScale = overlay.transform.localScale;
                     overlay.transform.localScale = new Vector2(overlayScale.x * AspectMultiplier, overlayScale.y);
                     overlayScale = new Vector2(0f,0f);
-                    Log.LogInfo($"Weather: Resized weather overlay {overlay.name}.");
+                    Log.LogInfo($"Weather: Resized weather overlay - {overlay.name}.");
                 }
             }
 
-            // Fix UI Camera aspect ratio
-            [HarmonyPatch(typeof(GwentUnity.AspectRatioManager), nameof(GwentUnity.AspectRatioManager.SetupAspectRatio))]
+            // Fix marker offset
+            [HarmonyPatch(typeof(GwentVisuals.Campaign.WorldPositionTracker), nameof(GwentVisuals.Campaign.WorldPositionTracker.Awake))]
             [HarmonyPostfix]
-            public static void AspectRatio(GwentUnity.AspectRatioManager __instance)
+            public static void FixMarkerOffset(GwentVisuals.Campaign.WorldPositionTracker __instance)
             {
-                __instance.m_RatioToMaintain = NewAspectRatio;
+                __instance.m_PanelRectSize = GameObject.Find("MainCanvas").GetComponent<RectTransform>().sizeDelta;
+                Log.LogInfo($"MarkerFix: Adjusted panel rect size - {__instance.gameObject.name}");
+            }
 
-                if (CrimsonUI.CrimsonUIManager.Instance != null)
-                {
-                    var UICamera = CrimsonUI.CrimsonUIManager.Instance.UICamera;
-                    UICamera.aspect = NewAspectRatio;
-                    UICamera.pixelRect = new Rect(0f, 0f, 1f, 1f);
-                    UICamera.rect = new Rect(0f, 0f, 1f, 1f);
-                }
-
-                Log.LogInfo($"AspectRatioManager: Adjusted target aspect ratio and UI Camera.");
+            // Fix video playback
+            [HarmonyPatch(typeof(GwentUnity.CutsceneMoviePlayer.CutscenePlayer), nameof(GwentUnity.CutsceneMoviePlayer.CutscenePlayer.OnMovieStarted))]
+            [HarmonyPostfix]
+            public static void FixVideo(GwentUnity.CutsceneMoviePlayer.CutscenePlayer __instance)
+            {
+                __instance.m_MoviePlayer.m_Player.aspectRatio = UnityEngine.Video.VideoAspectRatio.FitVertically;
+                Log.LogInfo($"VideoFix: Set aspect ratio to FitVertically - {__instance.m_MoviePlayer.MovieName}.");
             }
         }
 
